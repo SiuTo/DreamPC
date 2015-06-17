0695a9168c8a37e45dc0298f215cbeec6484be07
Xiaotao Liang <418xiaotao@163.com>
2015-06-17 23:16:31 +0800

    modify rfsrc model

--- rfsrc/cross_validate.R
+++ rfsrc/cross_validate.R
@@ -10,21 +10,21 @@ dtest <- read.csv("data_test.csv")
 dreal <- read.csv("real_value.csv")
 
 "Training..."
-rf.obj <- rfsrc(Surv(LKADT_P, DEATH) ~ ., dtrain, nsplit=10)
+rf.obj <- rfsrc(Surv(LKADT_P, DEATH) ~ ., dtrain, nsplit=3, importance="none")
 
 "Predicting..."
 rf.pred <- predict(rf.obj, dtest)
 l <- min(rf.pred$predicted)-10
 h <- max(rf.pred$predicted)+10
-dpred_1a <- (h-rf.pred$predicted)/(h-l)
+dpred_1a <- round((h-rf.pred$predicted)/(h-l), 3)
 write.table(dpred_1a, "pred_1a.txt", quote=FALSE, row.names=FALSE, col.names=FALSE)
-dpred_1b <- rf.pred$predicted
+dpred_1b <- round(rf.pred$predicted)
 write.table(dpred_1b, "pred_1b.txt", quote=FALSE, row.names=FALSE, col.names=FALSE)
 
 "Evaluating..."
 a <- score_q1a(dreal[, "LKADT_P"], dreal[, "DEATH"], dpred_1a)
 b <- score_q1b(dpred_1b, dreal[, "LKADT_P"], dreal[, "DEATH"])
-print(paste(round(a$cIndex, 3), round(a$auc12, 3), round(a$auc18, 3), round(a$auc24, 3), round(a$iAUC, 3), round(b, 3), sep=" "), quote=FALSE)
+cat(round(a$cIndex, 3), round(a$auc12, 3), round(a$auc18, 3), round(a$auc24, 3), round(a$iAUC, 3), round(b, 3), "\n", file="result.txt", append=TRUE)
 
 "Finish."
 
--- rfsrc/cross_validate.sh
+++ rfsrc/cross_validate.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
 
-for ((i=0; i<1; ++i)) do
+rm -f result.txt
+for ((i=0; i<3; ++i)) do
 	cp "data_train_$i.csv" "data_train.csv"
 	cp "data_test_$i.csv" "data_test.csv"
 	cp "real_value_$i.csv" "real_value.csv"
 	./cross_validate.R
-	mv "data_pred_1a.txt" "data_pred_1a_$i.txt"
-	mv "data_pred_1b.txt" "data_pred_1b_$i.txt"
+	mv "pred_1a.txt" "pred_1a_$i.txt"
+	mv "pred_1b.txt" "pred_1b_$i.txt"
 done
 
