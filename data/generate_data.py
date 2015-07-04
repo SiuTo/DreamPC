cc4aabb64e09ab7231a5f2de73a56d8e61316e75
Xiaotao Liang <418xiaotao@163.com>
2015-07-04 21:43:51 +0800

    deal with missing values

--- data/generate_data.py
+++ data/generate_data.py
@@ -16,8 +16,6 @@ def loadData(dataFileName, flag):
 		cnt = 0
 		for i in range(1, m):
 			if data[i][j]=="" or data[i][j]==">=85" or data[i][j]=="Missing" or data[i][j]=="MISSING" or data[i][j]==".":
-				if data[i][j]=='.':
-					data[i][j] = "0"
 				cnt += 1
 		if j==4 or j>=54:
 			distribution.append([data[0][j], 0, m-1-cnt])
@@ -28,77 +26,77 @@ def loadData(dataFileName, flag):
 	for i in range(1, m):
 		# DEATH
 		if data[i][4]=="":
-			data[i][4] = 0
+			data[i][4] = "0"
 		else:
-			data[i][4] = 1
+			data[i][4] = "1"
 
 		# AGEGRP2
 		if data[i][14]=="18-64":
-			data[i][14] = 1
+			data[i][14] = "1"
 		elif data[i][14]=="65-74":
-			data[i][14] = 2
+			data[i][14] = "2"
 		elif data[i][14]==">=75":
-			data[i][14] = 3
+			data[i][14] = "3"
 
 		# RACE_C
 		if data[i][15]=="Missing":
-			data[i][15] = 0
+			data[i][15] = "0"
 		elif data[i][15]=="Asian":
-			data[i][15] = 1
+			data[i][15] = "1"
 		elif data[i][15]=="Black":
-			data[i][15] = 2
+			data[i][15] = "2"
 		elif data[i][15]=="White":
-			data[i][15] = 3
+			data[i][15] = "3"
 		elif data[i][15]=="Hispanic":
-			data[i][15] = 4
+			data[i][15] = "4"
 		elif data[i][15]=="Other":
-			data[i][15] = 5
+			data[i][15] = "5"
 
 		# HGTBLCAT
 		if data[i][18]=="":
-			if data[i][17]=="0":
-				data[i][18] = 0
+			if data[i][17]==".":
+				data[i][18] = "."
 			else:
-				data[i][18] = floor(float(data[i][17])/20)
+				data[i][18] = str(floor(float(data[i][17])/20))
 		else:
-			data[i][18] = int(data[i][18][2:4])//2
+			data[i][18] = str(int(data[i][18][2:4])//2)
 		
 		# WGTBLCAT
 		if data[i][20]=="":
-			if data[i][19]=="0":
-				data[i][20] = 0
+			if data[i][19]==".":
+				data[i][20] = "."
 			else:
-				data[i][20] = floor(float(data[i][19])/10)
+				data[i][20] = str(floor(float(data[i][19])/10))
 		else:
 			if data[i][20][4]=="-":
-				data[i][20] = int(data[i][20][2:4])//10
+				data[i][20] = str(int(data[i][20][2:4])//10)
 			else:
-				data[i][20] = int(data[i][20][2:5])//10
+				data[i][20] = str(int(data[i][20][2:5])//10)
 
 		# REGION_C
 		if data[i][21]=="MISSING":
-			data[i][21] = 0
+			data[i][21] = "0"
 		elif data[i][21]=="AFRICA":
-			data[i][21] = 1
+			data[i][21] = "1"
 		elif data[i][21]=="ASIA/PACIFIC":
-			data[i][21] = 2
+			data[i][21] = "2"
 		elif data[i][21]=="EASTERN EUROPE":
-			data[i][21] = 3
+			data[i][21] = "3"
 		elif data[i][21]=="WESTERN EUROPE":
-			data[i][21] = 4
+			data[i][21] = "4"
 		elif data[i][21]=="NORTH AMERICA":
-			data[i][21] = 5
+			data[i][21] = "5"
 		elif data[i][21]=="SOUTH AMERICA":
-			data[i][21] = 6
+			data[i][21] = "6"
 		elif data[i][21]=="OTHER":
-			data[i][21] = 7
+			data[i][21] = "7"
 
 		# NON_TARGET-MHVASC
 		for j in range(54, 131):
 			if data[i][j]=="":
-				data[i][j] = 0
+				data[i][j] = "0"
 			else:
-				data[i][j] = 1
+				data[i][j] = "1"
 
 	featureFile = open("features_list.txt", "r")
 	feature = {}
@@ -115,9 +113,12 @@ def loadData(dataFileName, flag):
 		if flag=="train":
 			miss = False
 			for x in choose:
-				if 5<=x<=53 and x!=21 and data[i][x]==0:
-					miss = True
-					break
+				if data[i][x]==".":
+					if data[0][x] in ["BMI", "HGTBLCAT", "WGTBLCAT", "ECOG_C", "ALP", "ALT", "AST", "CA", "CREAT", "HB", "NEU", "PLT", "PSA", "TBILI", "WBC"]:
+						miss = True
+						break
+					else:
+						data[i][x] = "0"
 			if miss:
 				continue
 		rows.append([data[i][x] for x in choose])
--- rfsrc/select_features.R
+++ rfsrc/select_features.R
@@ -5,7 +5,7 @@ library("randomForestSRC")
 options(rf.cores=detectCores()-1, mc.cores=detectCores()-1)
 
 data <- read.csv("../data/data_train.csv")
-rf.obj <- rfsrc(Surv(LKADT_P, DEATH) ~ ., data, nsplit=3, importance="none")
+rf.obj <- rfsrc(Surv(LKADT_P, DEATH) ~ ., data, nsplit=2)
 topvars <- var.select(object = rf.obj)$topvars
 write.table(topvars, "features_list.txt", quote=FALSE, row.names=FALSE, col.names=FALSE)
 
