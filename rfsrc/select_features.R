0c08de4982f254e4d43bf763a755aa61dcc0e527
Xiaotao Liang <418xiaotao@163.com>
2015-07-05 20:50:29 +0800

    modify how to select

--- rfsrc/select_features.R
+++ rfsrc/select_features.R
@@ -5,6 +5,7 @@ library("randomForestSRC")
 options(rf.cores=detectCores()-1, mc.cores=detectCores()-1)
 
 data <- read.csv("../data/data_train.csv")
+data <- impute.rfsrc(Surv(LKADT_P, DEATH) ~ ., data, nsplit=2)
 rf.obj <- rfsrc(Surv(LKADT_P, DEATH) ~ ., data, nsplit=2)
 topvars <- var.select(object = rf.obj)$topvars
 write.table(topvars, "features_list.txt", quote=FALSE, row.names=FALSE, col.names=FALSE)
