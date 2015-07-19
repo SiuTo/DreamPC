8782c1fcd08c5226d23e04e3971cf42c4a6b0b93
Xiaotao Liang <418xiaotao@163.com>
2015-07-19 22:49:25 +0800

    train with regularized cox model

---
+++ glm/train_1.R
@@ -0,0 +1,21 @@
+#! /usr/bin/env Rscript
+
+library("glmnet")
+
+train <- function(dtrain, dtest)
+{
+	cat("\tTraining...\n")
+	x <- as.matrix(dtrain[, -(1:2)])
+	y <- cbind(time=dtrain[, "LKADT_P"], status=dtrain[, "DEATH"])
+	fit <- glmnet(x, y, family="cox")
+	cv.fit <- cv.glmnet(x, y, family="cox", nfold=5)
+	lambda <- cv.fit$lambda.min
+
+	cat("\tPredicting...\n")
+	xx <- as.matrix(dtest[, -(1:2)])
+	dpred_1a <- predict(fit, newx=xx, s=lambda, type="response")
+	dpred_1b <- predict(fit, newx=xx, s=lambda, type="link")
+
+	return(list(a=dpred_1a, b=dpred_1b))
+}
+
